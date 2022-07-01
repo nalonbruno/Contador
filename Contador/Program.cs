e85ea181421a9f9f23fd2b8f578951bc4882a270
Bruno Nalon Pereira <nalonbruno@yahoo.com.br>
2022-06-30 22:19:55 -0300

    Change

--- Contador/Program.cs
+++ Contador/Program.cs
@@ -10,6 +10,24 @@ namespace Contador
     {
         static void Main(string[] args)
         {
+            string val;
+            Console.WriteLine("Informe um número inteiro qualquer para somar seus múltiplos de 3 e 5:");
+            val = Console.ReadLine();
+            int num = Convert.ToInt32(val);
+            int total = 0;           
+            
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    total += i;
+                }
+                
+            }
+
+            Console.WriteLine("A soma dos múltiplos de 3 e 5 do número {0} é:{1}", num, total);
+
+
         }
     }
 }
